504e9a05f36d0464d3e09617b581f8a6027b1460
nobody <nobody@259782b6-b8a9-11dd-9634-b9af933a1305>
2011-11-01 10:12:52 +0000

    updatechecker implemented

--- IrMaster/trunk/src/IrMaster/IrMasterUtils.java
+++ IrMaster/trunk/src/IrMaster/IrMasterUtils.java
@@ -28,13 +28,14 @@ public class IrMasterUtils {
     public final static String third_party_string = "Makehex was written by John S. Fine (see LICENSE_makehex.txt), and has been translated to Java by Bengt Martensson. "
             + "ExchangeIR was written by Graham Dixon and published under GPL3 license. Its Analyze-function has been translated to Java by Bengt Martensson. "
             + "DecodeIR was originally written by John S. Fine, with later contributions from others. It is free software with undetermined license. "
-            + "IrpMaster depends on the runtime functions of ANTLR3, which is free software with BSD license.3 ";
+            + "IrpMaster depends on the runtime functions of ANTLR3, which is free software with BSD license.";
     public final static int main_version = 0;
     public final static int sub_version = 1;
     public final static int subminor_version = 1;
     public final static String version_suffix = "";
     public final static String version_string = "IrMaster version " + main_version
-            + "." + sub_version + "." + subminor_version + version_suffix;  
-    public final static String homepageUrl = "www.harctoolbox.org";
+            + "." + sub_version + "." + subminor_version + version_suffix;
+    public final static String homepageUrl = "http://www.harctoolbox.org";
+    public final static String currentVersionUrl = homepageUrl + "/downloads/irmaster.version";
     public final static long invalid = -1;
 }
