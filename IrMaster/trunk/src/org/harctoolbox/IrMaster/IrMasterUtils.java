8779781322e3101ddbe7bc11b64286135bd1536a
nobody <nobody@259782b6-b8a9-11dd-9634-b9af933a1305>
2012-04-13 17:17:06 +0000

    Updated copyright.

--- IrMaster/trunk/src/org/harctoolbox/IrMaster/IrMasterUtils.java
+++ IrMaster/trunk/src/org/harctoolbox/IrMaster/IrMasterUtils.java
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2011 Bengt Martensson.
+Copyright (C) 2011, 2012 Bengt Martensson.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@ package org.harctoolbox.IrMaster;
  *
  */
 public class IrMasterUtils {
-    public final static String license_string = "Copyright (C) 2011 Bengt Martensson.\n\n"
+    public final static String license_string = "Copyright (C) 2011, 2012 Bengt Martensson.\n\n"
             + "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.\n\n"
             + "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.\n\n"
             + "You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.";
