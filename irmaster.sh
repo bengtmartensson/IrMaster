7a85efe3d0561f106c16cc9a825b13e1ef94997b
Bengt Martensson <bengtmartensson@users.noreply.github.com>
2012-04-20 19:28:47 +0000

    Misc maintainence fixes

--- irmaster.sh
+++ irmaster.sh
@@ -3,14 +3,17 @@
 # Wrapper for IrMaster for Unix-like systems, feel free to adapt to your needs.
 
 # Preferred Java VM
-JAVA=/opt/jdk1.6.0_25/bin/java
+JAVA=/opt/jdk1.6.0_30/bin/java
 
 # Where IrMaster is installed
 IRMASTERHOME=/usr/local/irmaster
+
+# Use DecodeIR from the system of no private version is found
+#LIBRARY_PATH=/usr/local/lib
 
 # cd to the installation director to get the relative path names in
 # the default properties to fit, can be omitted if making file names
 # in the properties absolute.
 cd ${IRMASTERHOME}
 
-${JAVA} -jar ${IRMASTERHOME}/IrMaster.jar -p ${HOME}/.irmaster.properties.xml "$@"
+${JAVA} -Djava.library.path=${LIBRARY_PATH} -jar ${IRMASTERHOME}/IrMaster.jar -p ${HOME}/.irmaster.properties.xml "$@"
