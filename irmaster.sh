a0fbaa59a9b4608286ddc6336d839787ea38471a
Bengt Martensson <bengtmartensson@users.noreply.github.com>
2012-11-25 12:27:05 +0000

    Minor changes for release of version 0.3.1

--- irmaster.sh
+++ irmaster.sh
@@ -3,15 +3,20 @@
 # This wrapper is used to start both IrMaster and IrpMaster,
 # depending on what name it is called.
 
-# Intended for Unix-like systems, may need
-# to be locally adapted.
+# Intended for Unix-like systems (like Linux and MacOsX),
+# may need to be locally adapted.
+
+# When changing this file, it is a good idea to 
+# delete ~/.IrMaster.properties.xml
 
 # Preferred Java VM
 JAVA=java
 #JAVA=/opt/jdk1.6.0_30/bin/java
 
 # Where IrMaster is installed
-IRMASTERHOME=/usr/local/irmaster
+IRMASTERHOME=`dirname $0`
+#IRMASTERHOME=`pwd`
+#IRMASTERHOME=/usr/local/irmaster
 
 # Path to DecodeIR
 # If the code below does not work, just set LIBRARY_PATH to the directory
