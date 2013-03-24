27bdb74521679ee07524432a4a46ec0d933d47b0
nobody <nobody@259782b6-b8a9-11dd-9634-b9af933a1305>
2013-03-24 11:17:22 +0000

    tagged silly version, nuked

--- IrMaster/tags/0.3.2/tools/mksource.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-VERSION=`sed -e "s/IrMaster version //" irmaster.version`
-ZIPFILE=IrMaster-src-$VERSION.zip
-
-rm -f $ZIPFILE
-zip $ZIPFILE docs/* irmaster.sh src/org/harctoolbox/IrMaster/*.java src/org/harctoolbox/IrMaster/*.form nbproject/* build.xml manifest.mf irmaster.sh
