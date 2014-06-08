5fda33080db254120d0737c7bf7e68dfca498ec9
Bengt Martensson <bengtmartensson@users.noreply.github.com>
2014-06-08 13:38:53 +0000

    Reorganized makefiles

---
+++ tools/env.mk
@@ -0,0 +1,15 @@
+PROJECT_PREFIX=org/harctoolbox
+WWW_DIR := $(HOME)/harctoolbox/www
+DISTDIR := $(WWW_DIR)/src/content/xdocs/downloads
+JAVADOC_INSTALLDIR := /srv/www/htdocs/javadoc/$(PROJECT_PREFIX)
+ANT := ant
+#MAKE := make
+ZIP := zip -x \*~ @ 
+RM := rm -f
+export JAVA_HOME := /opt/jdk1.7.0_45
+export JAVA := $(JAVA_HOME)/bin/java
+XALAN := $(JAVA) -jar /usr/local/apache-forrest-0.9/lib/endorsed/xalan-2.7.1.jar
+
+TOOLS := tools
+VERSION_XML = programdata/$(PROJECT_PREFIX)/$(APPLICATION)/Version.xml
+
---
+++ tools/makerules.mk
@@ -0,0 +1,73 @@
+include version.mk
+
+version.mk: $(VERSION_XML)
+	$(XALAN) -XSL $(TOOLS)/mkVersionMkFile.xsl -IN $< -OUT $@
+
+version: $(APPLICATION).version
+
+$(APPLICATION).version: $(VERSION_XML)
+	$(XALAN) -XSL $(TOOLS)/mkVersionFile.xsl -IN $< -OUT $@
+
+SRC-DIST = $(APPLICATION)-src-$(VERSION).zip
+ifeq ($(BIN_DIST_FILES),)
+BIN-DIST=
+else
+BIN-DIST = $(APPLICATION)-bin-$(VERSION).zip
+endif
+
+.PHONY: docu clean veryclean all ant src-dist bin-dist version
+
+all: import $(APPLICATION).version dist/$(APPLICATION).jar docu src-dist bin-dist
+
+ant dist/$(APPLICATION).jar: import
+	$(ANT)
+
+import: $(foreach proj,$(IMPORT_PROJS),lib/$(proj).jar)
+
+VPATH=$(foreach proj,$(IMPORT_PROJS),../$(proj))
+
+lib/%.jar: dist/%.jar
+	cp $< $@
+
+docu: doc/$(APPLICATION).html
+
+doc/$(APPLICATION).html: doc/$(APPLICATION).xml $(TOOLS)/xdoc2html.xsl
+	$(XALAN) -XSL $(TOOLS)/xdoc2html.xsl -IN $< -OUT $@
+
+doc/%.pdf: $(WWW_DIR)/build/site/en/%.pdf
+	cp $< $@
+
+src-dist: $(SRC-DIST)
+
+#$(APPLICATION).version: ant
+#	$(JAVA) -classpath dist/$(APPLICATION).jar org.harctoolbox.$(APPLICATION).Version
+
+$(SRC-DIST): $(SRC-DIST-FILES)
+	$(RM) $@
+	$(ZIP) $@ $(SRC-DIST-FILES)
+
+ifneq ($(BIN_DIST_FILES),)
+bin-dist: $(BIN-DIST)
+
+$(BIN-DIST): $(BIN_DIST_FILES)
+	-rm -f $@
+	$(ZIP) $@ $(BIN_DIST_FILES)
+#	(cd data; $(ZIP) ../$@ IrpProtocols.ini )
+#	(cd dist; $(ZIP) ../$@ $(APPLICATION).jar lib/*)
+else
+bin-dist:
+	@echo No bin-dist exists for $(APPLICATION)
+endif
+
+export: $(SRC-DIST) $(BIN-DIST)
+	cp $^ $(DISTDIR)
+
+clean:
+	$(RM) -r $(SRC-DIST) $(BIN-DIST) dist doc/$(APPLICATION).html
+
+veryclean: clean
+	$(RM) $(APPLICATION).version version.mk
+
+install-javadoc: ant
+	$(RM) -r $(JAVADOC_INSTALLDIR)/$(APPLICATION)
+	cp -a dist/javadoc $(JAVADOC_INSTALLDIR)/$(APPLICATION)
