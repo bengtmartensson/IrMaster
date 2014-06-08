081f33ddc7a57cf5c277d4b4160370429012b6c1
nobody <nobody@259782b6-b8a9-11dd-9634-b9af933a1305>
2014-06-08 14:20:17 +0000

    added Inno compiler

--- IrMaster/trunk/tools/env.mk
+++ IrMaster/trunk/tools/env.mk
@@ -9,7 +9,7 @@ RM := rm -f
 export JAVA_HOME := /opt/jdk1.7.0_45
 export JAVA := $(JAVA_HOME)/bin/java
 XALAN := $(JAVA) -jar /usr/local/apache-forrest-0.9/lib/endorsed/xalan-2.7.1.jar
+INNO_COMPILER=c:\\Program Files\\Inno Setup 5\\ISCC.exe
 
 TOOLS := tools
 VERSION_XML = programdata/$(PROJECT_PREFIX)/$(APPLICATION)/Version.xml
-
