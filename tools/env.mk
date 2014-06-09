499ab2389ed399ad6c2a1320c17d5fe73ec688e1
Bengt Martensson <bengtmartensson@users.noreply.github.com>
2014-06-09 14:52:10 +0000

    More fixes to the make

--- tools/env.mk
+++ tools/env.mk
@@ -1,15 +1,17 @@
 PROJECT_PREFIX=org/harctoolbox
-WWW_DIR := $(HOME)/harctoolbox/www
+WWW_DIR := $(HOME)/harc/www.harctoolbox.org
+PDF_DIR := $(WWW_DIR)/build/site/en
 DISTDIR := $(WWW_DIR)/src/content/xdocs/downloads
 JAVADOC_INSTALLDIR := /srv/www/htdocs/javadoc/$(PROJECT_PREFIX)
 ANT := ant
 #MAKE := make
-ZIP := zip -x \*~ @ 
+ZIP := zip -9 -r -x \*~ \*\*/.svn\*\* @ 
 RM := rm -f
+TAR := tar
 export JAVA_HOME := /opt/jdk1.7.0_45
 export JAVA := $(JAVA_HOME)/bin/java
 XALAN := $(JAVA) -jar /usr/local/apache-forrest-0.9/lib/endorsed/xalan-2.7.1.jar
 INNO_COMPILER=c:\\Program Files\\Inno Setup 5\\ISCC.exe
 
 TOOLS := tools
-VERSION_XML = programdata/$(PROJECT_PREFIX)/$(APPLICATION)/Version.xml
+VERSION_XML = programdata/$(PROJECT_PREFIX)/$(PACKAGE)/Version.xml
